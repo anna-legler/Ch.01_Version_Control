a24788a0108e807c39fb97195a17d3a65e2e612e
leglerann <leglerann@urbandaleschools.com>
2022-09-07 08:05:26 -0500

    All done :D

--- 1.1_turtorial.py
+++ 1.1_turtorial.py
@@ -16,27 +16,68 @@ yoda.speed(10)  # speed of drawing. Go fast to not waste time.
 yoda.penup()
 yoda.goto(-30,-60)
 yoda.pendown()
-yoda.end_fill()
 yoda.color("orange")
+yoda.begin_fill()
 yoda.circle(80)#head
-yoda.goto(-180, 0)#rest of the body
+yoda.end_fill()
+yoda.begin_fill()#body start
+yoda.goto(-180, 0)
 yoda.goto(-180, 40)
 yoda.goto(-30, 100)
+yoda.end_fill()#body end
 yoda.penup()
 yoda.goto(-180, 0)#preparing to make tail
 yoda.pendown()
-yoda.goto(-220, -40)#tail
+yoda.begin_fill()#tail start
+yoda.goto(-220, -40)
 yoda.goto(-220,80)
 yoda.goto(-180, 40)
+yoda.end_fill()#tail end
 yoda.penup()
 yoda.goto(-30,100)#preperation to take top fin
 yoda.pendown()
-yoda.goto(-50, 160)
+yoda.begin_fill()#top fin start
+yoda.goto(-80, 140)
+yoda.goto(-130,140)
+yoda.goto(-100,120)
+yoda.goto(-110,68)
+yoda.end_fill()#top fin end
 yoda.penup()
+yoda.color("#ff8800")#preparing for side fin
+yoda.goto(-40,60)
+yoda.pendown()#side fin start
+yoda.goto(-100,45)
+yoda.goto(-120,-10)
+yoda.goto(-90,10)
+yoda.goto(-40,0)
+yoda.penup()#side fin end
+yoda.goto(0,30)#whites of eye preperation
+yoda.color("white")
+yoda.pendown()#whites of eyes start
+yoda.begin_fill()
+yoda.circle(20)
+yoda.end_fill()
+yoda.penup()#whites of eyes end
+yoda.goto(0,35)#pupil preperation
+yoda.color("black")
+yoda.pendown()
+yoda.begin_fill()
+yoda.circle(15)
+yoda.end_fill()
+yoda.penup()#pupil end
+yoda.goto(8,50)#eye shine preperation
+yoda.color("white")
+yoda.pendown()#eye shine start
+yoda.begin_fill()
+yoda.circle(3)
+yoda.end_fill()#eyeshine end
+yoda.penup()#signature prep
 yoda.setpos(200,-300)
 yoda.pendown()
-yoda.pencolor('#00FF00')
+yoda.pencolor('white')
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+
+yoda.write('Anna Legler',font=("Comic Sans", 16, "normal"))
+# signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
